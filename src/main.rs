ff03668cb6ceafdc32d4bb55efb38f761a3eb3a7
84k491 <bakar1337@gmail.com>
2024-11-16 17:05:22 +0400

    Remove dirs without music files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use audiotags::Tag;
 use clap::Parser;
 use pathdiff::diff_paths;
-use remove_empty_subdirs::remove_empty_subdirs;
 use std::fs;
 use std::path::PathBuf;
 use walkdir::WalkDir;
@@ -206,6 +205,58 @@ impl FileSorter {
     }
 }
 
+fn remove_non_music_direcotories(path: &PathBuf) -> bool {
+    let res = std::fs::read_dir(&path);
+    if let Err(err) = res {
+        println!("Can't read directory: {:?}, err: {}", &path, err);
+        return false;
+    }
+
+    let mut all_contents_removed = true;
+    let entries = res.unwrap();
+    for e in entries {
+        if let Err(err) = e {
+            println!("Can't read dir entry: {}", err);
+            continue;
+        }
+        let entry = e.unwrap();
+        let metadata_res = std::fs::metadata(entry.path());
+        if let Err(err) = metadata_res {
+            println!("Can't get metadata: {:?}, err: {}", entry.path(), err);
+            continue;
+        }
+        let metadata = metadata_res.unwrap();
+        if metadata.is_dir() {
+            let removed = remove_non_music_direcotories(&entry.path());
+            all_contents_removed = all_contents_removed & removed;
+            continue;
+        }
+
+        let has_tags = Tag::new().read_from_path(entry.path()).is_ok();
+        if has_tags {
+            all_contents_removed = false;
+            continue;
+        }
+
+        println!("Removing file: {:?}", entry.path());
+        if let Err(err) = std::fs::remove_file(entry.path()) {
+            println!("Can't remove a file: {:?}, err: {}", entry.path(), err);
+        }
+    }
+
+    if !all_contents_removed {
+        return false;
+    }
+
+    println!("Removing directory: {:?}", &path);
+    if let Err(err) = std::fs::remove_dir(&path) {
+        println!("Can't remove a directory: {:?}, err: {}", &path, err);
+        return false;
+    }
+
+    return true;
+}
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -229,5 +280,5 @@ fn main() {
     let base_path = std::env::current_dir().unwrap();
     let fs = FileSorter::new(&base_path, metainfo_source, args.dry_run);
     fs.sort();
-    let _ = remove_empty_subdirs(&base_path);
+    remove_non_music_direcotories(&base_path);
 }
